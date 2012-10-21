57599354f23dba5b26894b7c5f177c14693f6c8d
by-alex <dv.by.alex@gmail.com>
2012-10-20 19:40:58 -0700

    first commit

---
+++ TI_Rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TIRails::Application.config.secret_token = 'dd71e25a013d0a9493a0c331eda0f01c27e5319760d4b1ac330d72bff1e0f115df3555002a991cab69e69b935ca01c112418f9eccf03c926bd58c265dee6732f'
